41da130588a6d19c4676f4cc65b2b12248586510
Udesh Kumar <kudeshdav2107@gmail.com>
2022-03-02 18:01:10 +0530

    Second commit

--- first.java
+++ first.java
@@ -3,5 +3,7 @@ public class first
 {
     public static void main(String args[]){
         System.out.println("Welcome to GIT.");
+        System.out.println("Enjoy learning");
+        System.out.println("First change");
     }
 }
---
+++ second.java
@@ -0,0 +1,10 @@
+public class second {
+    public static void main(String args[])
+    {
+        int a=9;
+        int b=2;
+        System.out.println("Second file");
+        System.out.println(a-b);
+    }
+    
+}
