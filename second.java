2265fa4fc900c075b05f1493b0294b3c483cf43d
Udesh Kumar <kudeshdav2107@gmail.com>
2022-03-02 19:14:43 +0530

    new file

--- second.java
+++ second.java
@@ -2,7 +2,7 @@ public class second {
     public static void main(String args[])
     {
         int a=9;
-        int b=2;
+        int b=0;
         System.out.println("Second file");
         System.out.println(a-b);
     }
